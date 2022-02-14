8ab75b217f43302ea51e580a467674e4ee3a177c
zzy5201314 <1971166454@qq.com>
2022-02-14 23:06:04 +0800

    1.将system注册到eureka

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.course.controller;
+package com.course.system.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
