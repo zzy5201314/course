d88748f43379fe9cdf5b2abf2ddd31dbf393eca5
zzy5201314 <1971166454@qq.com>
2022-02-14 22:51:16 +0800

    1.将system配置成了最简单的SpringBoot项目

---
+++ system/src/main/java/com/course/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.course.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zzy
+ * @date 2022/2/14
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "system";
+    }
+}
