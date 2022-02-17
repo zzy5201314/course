4c10e78676c5aa221c2035502318a11d726003b3
zzy5201314 <1971166454@qq.com>
2022-02-17 22:36:52 +0800

    1.增加login页面 2.增加路由配置，访问/login时，懒加载login组件 3.vue-router版本为3.5.2，超过后不能使用目前的<router-view/>标签

---
+++ admin/src/router.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router  from 'vue-router'
+
+Vue.use(Router);
+
+export default new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [{
+        path: '*',
+        redirect: "/login",
+    },{
+        path: '/login',
+        // 懒加载
+        component: () => import("./views/login.vue")
+    }]
+})
