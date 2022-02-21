b65dc797de5207ee98031e21b4a6a6db34c3faf4
zzy5201314 <1971166454@qq.com>
2022-02-21 23:22:00 +0800

    1.增加business模块，并增加初始启动代码

---
+++ business/src/main/java/com/course/business/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.course.business.controller;
+
+import com.course.server.domain.Test;
+import com.course.server.service.TestService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author zzy
+ * @date 2022/2/14
+ */
+@RestController
+public class TestController {
+
+    @Resource
+    private TestService testService;
+
+    @RequestMapping("/test")
+    public List<Test> test(){
+        return testService.list();
+    }
+
+}
