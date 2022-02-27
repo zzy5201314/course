f44ca2c681ca4968ec4023ea2e2692f4629992f9
zzy5201314 <1971166454@qq.com>
2022-02-27 14:22:13 +0800

    1.增加了刷新功能 2.学会了live template模板的使用

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -4,6 +4,8 @@ import com.course.server.domain.Chapter;
 import com.course.server.dto.ChapterDto;
 import com.course.server.dto.PageDto;
 import com.course.server.service.ChapterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -16,6 +18,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin/chapter")
 public class ChapterController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ChapterController.class);
 
     @Resource
     private ChapterService chapterService;
